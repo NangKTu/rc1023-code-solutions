94a2f17fc05dc2bd4ead0ed529c3d3d2b8caa41d
Nang K Tu <ntu2391007@gmail.com>
2023-10-23 21:00:18 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 6;
+const height = 18;
+const area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+const bill = 10;
+const payment = 18;
+const change = payment - bill;
+console.log('value of : ', change);
+console.log('typeof change: ', typeof change);
+
+const quizzes = 80;
+const midterm = 99;
+const final = 100;
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+const firstName = 'Nang';
+const lastName = 'Tu';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName; ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+const pH = 7;
+const isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+const headCount = 100;
+const isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+let motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
