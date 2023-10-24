c9887d62585fa67de51d084e158572bee4e4c3ea
Nang K Tu <ntu2391007@gmail.com>
2023-10-24 17:10:46 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,11 @@
+const orderHistory = [
+  { orderNumber: '114-3941689-8772232', itemType: 'book', total: '34.00' },
+  { orderNumber: '113-9984268-1280257', itemType: 'book', total: '44.53' },
+  { orderNumber: '114-2875557-9059409', itemType: 'adapter', total: '17.22' },
+  {
+    orderNumber: '113-2883177-2648248',
+    itemType: ['controller', 'book'],
+    total: '138.93',
+  },
+];
+console.log(orderHistory);
