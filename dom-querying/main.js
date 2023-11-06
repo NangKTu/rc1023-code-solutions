271530c5a9155f48ba14b23a8c42518eeeb43b3d
Nang K Tu <ntu2391007@gmail.com>
2023-11-06 18:21:34 +0000

    Completed dom-querying - Nang Tu

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+console.log('hello, world');
+const h1Elem = document.querySelector('h1');
+console.log('h1 element:', h1Elem);
+console.dir(h1Elem);
+const idExplaination = document.querySelector('#explanation');
+console.log('id explanation:', idExplaination);
+console.dir(idExplaination);
+const classHint = document.querySelector('.hint');
+console.log('class hint:', classHint);
+console.dir(classHint);
+const para = document.querySelectorAll('p');
+console.log('paragrah:', para);
+const allClass = document.querySelectorAll('.example-link');
+console.log('links:', allClass);
