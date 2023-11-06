4c367f7d8b5f527febeb47ebe6006cfde900d0ba
Nang K Tu <ntu2391007@gmail.com>
2023-11-06 20:34:42 +0000

    Completed dom-events -Nang Tu

---
+++ dom-events/buttons.js
@@ -0,0 +1,23 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log(event);
+  console.log('event.target:', event.target);
+}
+const btnClick = document.querySelector('.click-button');
+btnClick.addEventListener('click', handleClick);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log(event);
+  console.log('event.target:', event.target);
+}
+const btnHover = document.querySelector('.hover-button');
+btnHover.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log(event);
+  console.log('event.target:', event.target);
+}
+const btnDouble = document.querySelector('.double-click-button');
+btnDouble.addEventListener('dblclick', handleDoubleClick);
