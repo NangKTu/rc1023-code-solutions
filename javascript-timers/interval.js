81374cf7de3bd6d463cdbbca64499d5948206a0a
Nang K Tu <ntu2391007@gmail.com>
2023-11-20 22:12:45 +0000

    update count

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,13 +1,12 @@
 const $countdownDisplay = document.querySelector('.countdown-display');
 let count = 4;
 function updateHeader() {
+  count--;
   $countdownDisplay.textContent = count;
-
   if (count === 0) {
     $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
     clearInterval(intervalID);
   }
-  count--;
 }
 
-const intervalID = setInterval(updateHeader, 1500);
+const intervalID = setInterval(updateHeader, 1000);
