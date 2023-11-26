259c466abe2329f68f917d38da54ed350476c4e3
Nang K Tu <ntu2391007@gmail.com>
2023-11-26 19:25:42 +0000

    now it is done

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,35 +1,37 @@
 const $textBody = document.querySelector('.phrase');
-const textMessage = 'grumpy wizards make toxic brew';
-const spanElements = [];
+const $textPhrase = 'grumpy wizards make toxic brew';
+const $span = [];
 let letterNum = 0;
 
-for (let i = 0; i < textMessage.length; i++) {
+for (let i = 0; i < $textPhrase.length; i++) {
   const newElement = document.createElement('span');
 
   newElement.className = 'text';
   if (i === 0) newElement.classList.add('selected');
-  const letter = textMessage[i];
+  const letter = $textPhrase[i];
   if (letter === ' ') {
     // run nothing
   }
   newElement.textContent = letter;
-  spanElements.push(newElement);
+  $span.push(newElement);
   $textBody.appendChild(newElement);
 }
 
 document.addEventListener('keydown', function (event) {
-  if (letterNum === spanElements.length) return;
+  if (letterNum === $span.length) {
+    return;
+  }
 
   const keyPress = event.key;
-  const correctKey = textMessage[letterNum];
-  const currentElement = spanElements[letterNum];
+  const correctKey = $textPhrase[letterNum];
+  const currentElement = $span[letterNum];
 
   if (keyPress === correctKey) {
     currentElement.classList.remove('incorrect');
     currentElement.classList.add('correct');
     currentElement.classList.remove('selected');
 
-    const nextElement = spanElements[letterNum + 1];
+    const nextElement = $span[letterNum + 1];
     if (nextElement) nextElement.classList.add('selected');
     letterNum++;
   } else {
