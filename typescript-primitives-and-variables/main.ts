2cd3db94eb27a290f2d69ff4b619b2375b3f6c1f
Nang K Tu <ntu2391007@gmail.com>
2023-12-06 19:17:35 +0000

    completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullName: string = 'Nang Tu';
+const isCool: boolean = true;
+const totalPets: number = 0;
+const unknown: null = null;
+const never: undefined = undefined;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeOf fullName:', typeof fullName);
+console.log('typeOf isCool:', typeof isCool);
+console.log('typeOf totalPets:', typeof totalPets);
+console.log('value of unknown:', unknown);
+console.log('typeOf unknown:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeOf never:', typeof never);
