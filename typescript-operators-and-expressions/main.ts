f77b30761383e812b7c35782733695f6b628d238
Nang K Tu <ntu2391007@gmail.com>
2023-12-06 20:14:51 +0000

    completed typescript-operators-and-expressions

---
+++ typescript-operators-and-expressions/main.ts
@@ -0,0 +1,38 @@
+const width: number = 20;
+const height: number = 11;
+const area: number = width * height;
+console.log('value of area:', area);
+console.log('typeOf area:', typeof area);
+
+const bill: number = 13.99;
+const payment: number = 20;
+const change: number = payment - bill;
+console.log('value of change:', change);
+console.log('typeOf change:', typeof change);
+
+const quizzes: number = 30;
+const midterm: number = 32;
+const final: number = 99;
+const grade: number = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeOf grade:', typeof grade);
+
+const firstName: string = 'Leon';
+const lastName: string = 'Kennedy';
+const fullName: string = `${firstName} ${lastName}`;
+console.log('value of fullName:', fullName);
+console.log('typeOf fullName:', typeof fullName);
+
+const pH: number = 6;
+const isAcidic: boolean = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeOf isAcidic:', typeof isAcidic);
+
+const headCount: number = 300;
+const isSparta: boolean = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeOf isSparta:', typeof isSparta);
+
+const motto: string = `${fullName} is the GOAT`;
+console.log('value of motto:', motto);
+console.log('typeOf motto:', typeof motto);
